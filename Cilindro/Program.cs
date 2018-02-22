6f0f4f244d72e37fea4b09fc4a66c2a822e4ced7
Ines <ines98goncalves@gmail.com>
2018-02-22 12:21:05 +0000

    Adicionar o codigo

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cilindro
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Cilindro {
+    class Program {
+        static void Main(string[] args) {
+            int a;
+            int r;
+            double v;
+            double sa;
+
+            string pa = "Escreve a altura do circulo.";
+            Console.WriteLine(pa);
+            string area = Console.ReadLine();
+
+            string pr = "Escreve o raio do circulo.";
+            Console.WriteLine(pr);
+            string raio = Console.ReadLine();
+
+            a = Convert.ToInt32(area);
+            r = Convert.ToInt32(raio);
+
+            v = Math.PI * r * r * a;
+            sa = 2 * Math.PI * r * (r + a);
+
+            Console.WriteLine("Volume:" + v);
+            Console.WriteLine("Area da Superficie:" + sa);
+
+
         }
     }
 }
